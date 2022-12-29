4be2a9cdf383f1ae15816e2f7cc5672c2f0fbabb
santhosh <santhosh.srinivas25@gmail.com>
2022-12-29 16:27:29 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Santhosh
           </a>
         </p>
       </header>
